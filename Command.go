b9f039d0eb4e711bf832efa89802133f9e1a97fc
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-09-04 18:45:32 -0500

    Remove unnecessary import

--- Command.go
+++ Command.go
@@ -24,7 +24,6 @@ package animatedledstrip
 
 import (
 	"encoding/json"
-	"log"
 )
 
 type Command struct {
--- Section.go
+++ Section.go
@@ -24,7 +24,6 @@ package animatedledstrip
 
 import (
 	"encoding/json"
-	"log"
 	"strings"
 )
 
